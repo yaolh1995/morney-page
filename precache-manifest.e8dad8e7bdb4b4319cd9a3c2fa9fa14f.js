72f1b409e70953bf5ac4cf965e468f9639e48c7d
姚霖宏 <7881785+ylh1995@user.noreply.gitee.com>
2020-08-27 23:36:52 +0800

    deploy

---
+++ precache-manifest.e8dad8e7bdb4b4319cd9a3c2fa9fa14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42865ab8b30f0b27943",
+    "url": "/morney-page/css/app.d0ea897f.css"
+  },
+  {
+    "revision": "390fccb69bdd1d949ed8ce27b1300955",
+    "url": "/morney-page/index.html"
+  },
+  {
+    "revision": "c42865ab8b30f0b27943",
+    "url": "/morney-page/js/app.2beed027.js"
+  },
+  {
+    "revision": "552a3ff87edd6914e4ad",
+    "url": "/morney-page/js/chunk-vendors.e65b4a08.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-page/robots.txt"
+  }
+]);
